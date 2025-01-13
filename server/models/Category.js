a3333a16fe271820c1ddda9fe6637c7d72439f13
Manav2031 <manavmehta2031@gmail.com>
2025-01-13 17:36:22 +0530

    updated code

--- server/models/Category.js
+++ server/models/Category.js
@@ -15,5 +15,44 @@ const categorySchema = new mongoose.Schema({
 	],
 });
 
-// Export the Tags model
-module.exports = mongoose.model("Category", categorySchema);
+
+const Category = mongoose.model("Category", categorySchema);
+
+const seedCategories = async () => {
+	const defaultCategories = [
+		{
+			name: "Programming",
+			description: "Courses related to programming languages and frameworks.",
+		},
+		{
+			name: "Data Science",
+			description: "Courses related to data analysis, machine learning, and AI.",
+		},
+		{
+			name: "Web Development",
+			description: "Courses on building websites and web applications.",
+		},
+		{
+			name: "Cybersecurity",
+			description: "Courses focused on protecting systems and data from cyber threats.",
+		},
+	];
+
+	for (const category of defaultCategories) {
+		try {
+			const existingCategory = await Category.findOne({ name: category.name });
+			if (!existingCategory) {
+				await Category.create(category);
+				console.log(`Category '${category.name}' added to the database.`);
+			} else {
+				console.log(`Category '${category.name}' already exists.`);
+			}
+		} catch (error) {
+			console.error(`Error adding category '${category.name}':`, error);
+		}
+	}
+};
+
+seedCategories();
+
+module.exports = Category;
